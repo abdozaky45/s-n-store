e696df0e10705339d176136b7a86b15e27ce3cbc
Abdulrahman Mohamed Zaki <102440301+abdozaky45@users.noreply.github.com>
2026-03-30 21:13:53 +0200

    feat: uncomment OrderController, OrderRouter, and OrderService code for functionality restoration

--- src/Router/Order/OrderRouter.ts
+++ src/Router/Order/OrderRouter.ts
@@ -5,9 +5,9 @@ import { Validation } from '../../middleware/ValidationMiddleware';
 import { baseSchema } from '../../Validation/baseSchema';
 
 const OrderRouter = Router();
-// OrderRouter.post("/create", Validation(orderValidation.createOrderValidation), OrderController.createOrder);
-// OrderRouter.patch("/update-status", Validation(orderValidation.updateOrderStatusValidation), OrderController.updateOrderStatus);
-// OrderRouter.get("/get-user-orders", Validation(baseSchema), OrderController.getUserOrders);
-// OrderRouter.get("/get-all-orders", Validation(orderValidation.getAllOrdersValidation), OrderController.getAllOrders);
-// OrderRouter.get("/get-order/:orderId", Validation(orderValidation.getOrderByIdValidation), OrderController.getOrderById);
+OrderRouter.post("/create", Validation(orderValidation.createOrderValidation), OrderController.createOrder);
+OrderRouter.patch("/update-status", Validation(orderValidation.updateOrderStatusValidation), OrderController.updateOrderStatus);
+OrderRouter.get("/get-user-orders", Validation(baseSchema), OrderController.getUserOrders);
+OrderRouter.get("/get-all-orders", Validation(orderValidation.getAllOrdersValidation), OrderController.getAllOrders);
+OrderRouter.get("/get-order/:orderId", Validation(orderValidation.getOrderByIdValidation), OrderController.getOrderById);
 export default OrderRouter;
