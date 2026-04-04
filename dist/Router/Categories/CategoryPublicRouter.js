608dff4fb19d7404f270532236102b3299b2caff
Abdulrahman Mohamed Zaki <102440301+abdozaky45@users.noreply.github.com>
2026-04-05 01:49:04 +0200

    Remove validation schemas and related middleware for offers, orders, products, shipping, size categories, social reviews, subcategories, user authentication, customer information, and variants. Also, delete the base schema and main application setup files, including CORS configuration and authentication middleware.

--- dist/Router/Categories/CategoryPublicRouter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const CategoryController_1 = require("../../Controller/Category/CategoryController");
-const categoryPublicRouter = (0, express_1.Router)();
-categoryPublicRouter.get("/get-all-categories", CategoryController_1.getCategories);
-categoryPublicRouter.get("/get-one-category/:categoryId", CategoryController_1.getCategoryById);
-exports.default = categoryPublicRouter;
--- dist/Router/SubCategory/SubCategoryPublicRouter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const SubCategoryController_1 = require("../../Controller/SubCategory/SubCategoryController");
-const subCategoryPublicRouter = (0, express_1.Router)();
-subCategoryPublicRouter.get("/get-all-sub-categories", SubCategoryController_1.getSubCategories);
-subCategoryPublicRouter.get("/get-one-sub-category/:subCategoryId", SubCategoryController_1.getSubCategoryById);
-exports.default = subCategoryPublicRouter;
