7cd9a46ee4f82ddfbde26cee9692e5a4d5134a3e
Abdulrahman Mohamed Zaki <102440301+abdozaky45@users.noreply.github.com>
2026-04-06 00:30:58 +0200

    feat: reorganize size category routes; streamline route definitions for improved clarity and consistency

--- src/Router/SizeCategory/SizeCategoryRouter.ts
+++ src/Router/SizeCategory/SizeCategoryRouter.ts
@@ -3,16 +3,19 @@ import * as SizeCategoryController from "../../Controller/SizeCategory/SizeCateg
 import { Validation } from '../../middleware/ValidationMiddleware';
 import * as SizeCategoryValidation from "../../Validation/SizeCategory/SizeCategoryValidation";
 const SizeCategoryRouter = Router();
+
 // Group Size Routes
+SizeCategoryRouter.get("/group-all", SizeCategoryController.getAllGroupSizes);
 SizeCategoryRouter.post("/group", Validation(SizeCategoryValidation.createGroupSize), SizeCategoryController.createNewGroupSize);
 SizeCategoryRouter.get("/group/:_id", Validation(SizeCategoryValidation.getGroupSizeById), SizeCategoryController.getGroupSizeById);
-SizeCategoryRouter.get("/group-all", SizeCategoryController.getAllGroupSizes);
-SizeCategoryRouter.patch("/update-group/:_id", Validation(SizeCategoryValidation.updateGroupSize), SizeCategoryController.updateGroupSizeById);    
+SizeCategoryRouter.patch("/update-group/:_id", Validation(SizeCategoryValidation.updateGroupSize), SizeCategoryController.updateGroupSizeById);
+
 // Size Category Routes
+SizeCategoryRouter.get("/all-size", SizeCategoryController.getAllSizeCategories);
+SizeCategoryRouter.get("/all-sizes-by-group/:groupId", Validation(SizeCategoryValidation.getSizeCategoriesByGroupId), SizeCategoryController.getSizeCategoriesByGroupId);
 SizeCategoryRouter.post("/size", Validation(SizeCategoryValidation.createSizeCategory), SizeCategoryController.createNewSizeCategory);
+SizeCategoryRouter.get("/one-size/:_id", Validation(SizeCategoryValidation.getSizeCategoryById), SizeCategoryController.getSizeCategoryById);
 SizeCategoryRouter.patch("/size/:_id", Validation(SizeCategoryValidation.updateSizeCategory), SizeCategoryController.updateSizeCategoryById);
 SizeCategoryRouter.delete("/size/:_id", Validation(SizeCategoryValidation.deleteSizeCategoryById), SizeCategoryController.deleteSizeCategoryById);
-SizeCategoryRouter.get("/all-size", SizeCategoryController.getAllSizeCategories);
-SizeCategoryRouter.get("/one-size/:_id", Validation(SizeCategoryValidation.getSizeCategoryById), SizeCategoryController.getSizeCategoryById);
-SizeCategoryRouter.get("/all-sizes-by-group/:groupId", Validation(SizeCategoryValidation.getSizeCategoriesByGroupId), SizeCategoryController.getSizeCategoriesByGroupId);
+
 export default SizeCategoryRouter;
