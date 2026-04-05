49616f9335fe133b2b0d94801372bc6e24969e8d
Abdulrahman Mohamed Zaki <102440301+abdozaky45@users.noreply.github.com>
2026-04-06 00:32:38 +0200

    feat: clean up size category router; remove unnecessary line breaks for improved readability

--- src/Router/SizeCategory/SizeCategoryRouter.ts
+++ src/Router/SizeCategory/SizeCategoryRouter.ts
@@ -3,13 +3,11 @@ import * as SizeCategoryController from "../../Controller/SizeCategory/SizeCateg
 import { Validation } from '../../middleware/ValidationMiddleware';
 import * as SizeCategoryValidation from "../../Validation/SizeCategory/SizeCategoryValidation";
 const SizeCategoryRouter = Router();
-
 // Group Size Routes
 SizeCategoryRouter.get("/group-all", SizeCategoryController.getAllGroupSizes);
 SizeCategoryRouter.post("/group", Validation(SizeCategoryValidation.createGroupSize), SizeCategoryController.createNewGroupSize);
 SizeCategoryRouter.get("/group/:_id", Validation(SizeCategoryValidation.getGroupSizeById), SizeCategoryController.getGroupSizeById);
 SizeCategoryRouter.patch("/update-group/:_id", Validation(SizeCategoryValidation.updateGroupSize), SizeCategoryController.updateGroupSizeById);
-
 // Size Category Routes
 SizeCategoryRouter.get("/all-size", SizeCategoryController.getAllSizeCategories);
 SizeCategoryRouter.get("/all-sizes-by-group/:groupId", Validation(SizeCategoryValidation.getSizeCategoriesByGroupId), SizeCategoryController.getSizeCategoriesByGroupId);
@@ -17,5 +15,4 @@ SizeCategoryRouter.post("/size", Validation(SizeCategoryValidation.createSizeCat
 SizeCategoryRouter.get("/one-size/:_id", Validation(SizeCategoryValidation.getSizeCategoryById), SizeCategoryController.getSizeCategoryById);
 SizeCategoryRouter.patch("/size/:_id", Validation(SizeCategoryValidation.updateSizeCategory), SizeCategoryController.updateSizeCategoryById);
 SizeCategoryRouter.delete("/size/:_id", Validation(SizeCategoryValidation.deleteSizeCategoryById), SizeCategoryController.deleteSizeCategoryById);
-
 export default SizeCategoryRouter;
