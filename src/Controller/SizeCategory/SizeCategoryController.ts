99f507ef3d50806f633371f94496e3298bab9cfe
Abdulrahman Mohamed Zaki <102440301+abdozaky45@users.noreply.github.com>
2026-04-06 01:20:55 +0200

    feat: refactor size category controller and service; standardize method names and improve import structure for consistency

--- src/Controller/SizeCategory/SizeCategoryController.ts
+++ src/Controller/SizeCategory/SizeCategoryController.ts
@@ -1,74 +1,72 @@
 import { Request, Response } from "express";
 import { ApiError, ApiResponse, asyncHandler } from "../../Utils/ErrorHandling";
 import ISizeCategory from "../../Model/SizeCategory/ISizeCategoryModel";
-import { createGroupSize, createSizeCategory, deleteSizeCategory, findAllGroupSizes, findAllSizeCategories, findGroupSizeById, findSizeCategoriesByGroupId, findSizeCategoryById, updateGroupSize, updateSizeCategory } from "../../Service/SizeCategory/SizeCategoryService";
+import * as SizeCategoryService from "../../Service/SizeCategory/SizeCategoryService";
 import SuccessMessage from "../../Utils/SuccessMessages";
 import ErrorMessages from "../../Utils/Error";
-export const createNewGroupSize = asyncHandler(async (req: Request, res: Response) => {
+import { checkGroupSizeExists, checkSizeCategoryExistsById } from "../../Shared/GroupSizeShared";
+export const createGroupSize = asyncHandler(async (req: Request, res: Response) => {
     const { name } = req.body as { name: string };
-    const groupSize = await createGroupSize(name);
+    const groupSize = await SizeCategoryService.createGroupSize(name);
     return res.status(201).json(new ApiResponse(201, { groupSize }, SuccessMessage.GROUP_SIZE_CREATED));
 });
 export const getGroupSizeById = asyncHandler(async (req: Request, res: Response) => {
     const { _id } = req.params as { _id: string };
-    const groupSize = await findGroupSizeById(_id);
+    const groupSize = await SizeCategoryService.getGroupSizeById(_id);
     if (!groupSize)
         throw new ApiError(400, ErrorMessages.GROUP_SIZE_NOT_FOUND);
     return res.status(200).json(new ApiResponse(200, { groupSize }, SuccessMessage.SIZE_GROUP_FETCHED));
 });
 export const getAllGroupSizes = asyncHandler(async (req: Request, res: Response) => {
-    const groupSizes = await findAllGroupSizes();
+    const groupSizes = await SizeCategoryService.getAllGroupSizes();
     return res.status(200).json(new ApiResponse(200, { groupSizes }, SuccessMessage.SIZE_GROUP_FETCHED));
 });
 export const updateGroupSizeById = asyncHandler(async (req: Request, res: Response) => {
     const { _id } = req.params as { _id: string };
     const { name } = req.body as { name: string };
-    const groupSize = await findGroupSizeById(_id);
-    if (!groupSize) throw new ApiError(400, ErrorMessages.GROUP_SIZE_NOT_FOUND);
-    const updatedGroupSize = await updateGroupSize(_id, name);
-    return res.status(200).json(new ApiResponse(200, { groupSize: updatedGroupSize }, SuccessMessage.GROUP_SIZE_UPDATED));
+    const groupSize = await SizeCategoryService.updateGroupSize(_id, name);
+      if (!groupSize) throw new ApiError(400, ErrorMessages.GROUP_SIZE_NOT_FOUND);
+    return res.status(200).json(new ApiResponse(200, { groupSize }, SuccessMessage.GROUP_SIZE_UPDATED));
 });
-
-export const createNewSizeCategory = asyncHandler(async (req: Request, res: Response) => {
+export const createSizeCategory = asyncHandler(async (req: Request, res: Response) => {
     const sizeCategoryData: ISizeCategory = req.body;
-    const groupSize = await findGroupSizeById(sizeCategoryData.groupSize);
+    const groupSize = await checkGroupSizeExists(sizeCategoryData.groupSize);
     if (!groupSize) throw new ApiError(400, ErrorMessages.GROUP_SIZE_NOT_FOUND);
-    const sizeCategory = await createSizeCategory(sizeCategoryData);
+    const sizeCategory = await SizeCategoryService.createSizeCategory(sizeCategoryData);
     return res.status(201).json(new ApiResponse(201, { sizeCategory }, SuccessMessage.SIZE_CATEGORY_CREATED));
 });
 export const getSizeCategoryById = asyncHandler(async (req: Request, res: Response) => {
     const { _id } = req.params as { _id: string };
-    const sizeCategory = await findSizeCategoryById(_id);
+    const sizeCategory = await SizeCategoryService.getSizeCategoryById(_id);
     if (!sizeCategory)
         throw new ApiError(400, ErrorMessages.SIZE_CATEGORY_NOT_FOUND);
     return res.status(200).json(new ApiResponse(200, { sizeCategory }, SuccessMessage.SIZE_CATEGORY_FETCHED));
 });
 export const getAllSizeCategories = asyncHandler(async (req: Request, res: Response) => {
-    const sizeCategories = await findAllSizeCategories();
+    const sizeCategories = await SizeCategoryService.getAllSizeCategories();
     return res.status(200).json(new ApiResponse(200, { sizeCategories }, SuccessMessage.SIZE_CATEGORY_FETCHED));
 });
 export const getSizeCategoriesByGroupId = asyncHandler(async (req: Request, res: Response) => {
     const { groupId } = req.params as { groupId: string };
-    const sizeCategories = await findSizeCategoriesByGroupId(groupId);
+    const sizeCategories = await SizeCategoryService.getSizeCategoriesByGroupId(groupId);
+    if (sizeCategories.length === 0) throw new ApiError(400, ErrorMessages.SIZE_CATEGORY_NOT_FOUND);
     return res.status(200).json(new ApiResponse(200, { sizeCategories }, SuccessMessage.SIZE_CATEGORY_FETCHED));
 });
 export const updateSizeCategoryById = asyncHandler(async (req: Request, res: Response) => {
     const { _id } = req.params as { _id: string };
     const sizeCategoryData: Partial<ISizeCategory> = req.body;
    if(sizeCategoryData.groupSize){
-    const groupSize = await findGroupSizeById(sizeCategoryData.groupSize);
+    const groupSize = await checkGroupSizeExists(sizeCategoryData.groupSize);
     if (!groupSize) throw new ApiError(400, ErrorMessages.GROUP_SIZE_NOT_FOUND);
    }
-    const sizeCategory = await findSizeCategoryById(_id);
+    const sizeCategory = await checkSizeCategoryExistsById(_id);
     if (!sizeCategory) throw new ApiError(400, ErrorMessages.SIZE_CATEGORY_NOT_FOUND);
-    const updatedSizeCategory = await updateSizeCategory(_id, sizeCategoryData);
+    const updatedSizeCategory = await SizeCategoryService.updateSizeCategory(_id, sizeCategoryData);
     return res.status(200).json(new ApiResponse(200, { sizeCategory: updatedSizeCategory }, SuccessMessage.SIZE_CATEGORY_UPDATED));
 });
 export const deleteSizeCategoryById = asyncHandler(async (req: Request, res: Response) => {
     const { _id } = req.params as { _id: string };
-    const sizeCategory = await deleteSizeCategory(_id);
+    const sizeCategory = await SizeCategoryService.deleteSizeCategory(_id);
     if (!sizeCategory) throw new ApiError(400, ErrorMessages.SIZE_CATEGORY_NOT_FOUND);
     return res.status(200).json(new ApiResponse(200, {}, SuccessMessage.SIZE_CATEGORY_DELETED_SUCCESS));
 });
-
-
--- src/Router/SizeCategory/SizeCategoryRouter.ts
+++ src/Router/SizeCategory/SizeCategoryRouter.ts
@@ -5,13 +5,13 @@ import * as SizeCategoryValidation from "../../Validation/SizeCategory/SizeCateg
 const SizeCategoryRouter = Router();
 // Group Size Routes
 SizeCategoryRouter.get("/group-all", SizeCategoryController.getAllGroupSizes);
-SizeCategoryRouter.post("/group", Validation(SizeCategoryValidation.createGroupSize), SizeCategoryController.createNewGroupSize);
+SizeCategoryRouter.post("/group", Validation(SizeCategoryValidation.createGroupSize), SizeCategoryController.createGroupSize);
 SizeCategoryRouter.get("/group/:_id", Validation(SizeCategoryValidation.getGroupSizeById), SizeCategoryController.getGroupSizeById);
 SizeCategoryRouter.patch("/update-group/:_id", Validation(SizeCategoryValidation.updateGroupSize), SizeCategoryController.updateGroupSizeById);
 // Size Category Routes
 SizeCategoryRouter.get("/all-size", SizeCategoryController.getAllSizeCategories);
 SizeCategoryRouter.get("/all-sizes-by-group/:groupId", Validation(SizeCategoryValidation.getSizeCategoriesByGroupId), SizeCategoryController.getSizeCategoriesByGroupId);
-SizeCategoryRouter.post("/size", Validation(SizeCategoryValidation.createSizeCategory), SizeCategoryController.createNewSizeCategory);
+SizeCategoryRouter.post("/size", Validation(SizeCategoryValidation.createSizeCategory), SizeCategoryController.createSizeCategory);
 SizeCategoryRouter.get("/one-size/:_id", Validation(SizeCategoryValidation.getSizeCategoryById), SizeCategoryController.getSizeCategoryById);
 SizeCategoryRouter.patch("/size/:_id", Validation(SizeCategoryValidation.updateSizeCategory), SizeCategoryController.updateSizeCategoryById);
 SizeCategoryRouter.delete("/size/:_id", Validation(SizeCategoryValidation.deleteSizeCategoryById), SizeCategoryController.deleteSizeCategoryById);
